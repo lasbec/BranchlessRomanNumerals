b18be7f8b88559c822fcd2f07dc74318e6f13273
Lasse Becker <lasse@cpsh.de>
2024-01-06 18:45:41 +0100

    small improvements in readability

--- parseRoman.zig
+++ parseRoman.zig
@@ -3,67 +3,71 @@ const ArgIterator = std.process.ArgIterator;
 const allocator = std.heap.page_allocator;
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
     var argv = try ArgIterator.initWithAllocator(allocator);
     defer ArgIterator.deinit(&argv);
-    const argv_p = &argv;
-    _ = ArgIterator.skip(argv_p);
-    var arg = ArgIterator.next(argv_p) orelse "";
-    const value = parseRoman(&arg);
-    try stdout.print("The value in decimal representation is: {d}\n", .{value});
+
+    _ = ArgIterator.skip(&argv); // first argument is own file location
+    var arg = ArgIterator.next(&argv) orelse "";
+    const result = parseRoman(&arg);
+
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("The value in decimal representation is: {d}\n", .{result});
 }
 
 pub fn parseRoman(input: *[:0]const u8) i32 {
     var result: i32 = 0;
     var prev: i32 = 0;
     var curr: i32 = 0;
+
     for (input.*) |byte| {
-        curr = romanDigit(byte);
-        const negate: i32 = @intFromBool(prev < curr);
-        result += prev - negate * 2 * prev;
+        curr = romanDigitValue(byte);
+        const doNegate: i2 = @intFromBool(prev < curr);
+        const sign: i32 = 1 - doNegate * 2;
+        result += prev * sign;
         prev = curr;
     }
+
     return result + curr;
 }
 
-fn romanDigit(char: u8) i32 {
-    const c = @as(i32, char);
-    const mod = @mod(c, 10);
-    const b4 = c & 8;
-    const b3 = c & 4;
-    const b2 = c & 2;
-    const b1 = c & 1;
-    return -2129 + @divFloor(c * 743 + mod * 4412 - b4 * 1890 + b3 * 4835 - b2 * 12175 + b1 * 32845, 40);
+fn romanDigitValue(char: u8) i32 {
+    const full = @as(i32, char);
+    const lastDigit = @mod(full, 10);
+    const byte4 = full & 8;
+    const byte3 = full & 4;
+    const byte2 = full & 2;
+    const byte1 = full & 1;
+    return -2129 + @divFloor(full * 743 + lastDigit * 4412 - byte4 * 1890 + byte3 * 4835 - byte2 * 12175 + byte1 * 32845, 40);
 }
 
 test "I = 1" {
-    const result = romanDigit('I');
+    const result = romanDigitValue('I');
     try std.testing.expectEqual(result, 1);
 }
 
 test "V = 5" {
-    const result = romanDigit('V');
+    const result = romanDigitValue('V');
     try std.testing.expectEqual(result, 5);
 }
 test "X = 10" {
-    const result = romanDigit('X');
+    const result = romanDigitValue('X');
     try std.testing.expectEqual(result, 10);
 }
 test "L = 50" {
-    const result = romanDigit('L');
+    const result = romanDigitValue('L');
     try std.testing.expectEqual(result, 50);
 }
 test "C = 100" {
-    const result = romanDigit('C');
+    const result = romanDigitValue('C');
     try std.testing.expectEqual(result, 100);
 }
 
 test "D = 500" {
-    const result = romanDigit('D');
+    const result = romanDigitValue('D');
     try std.testing.expectEqual(result, 500);
 }
 
 test "M = 1000" {
-    const result = romanDigit('M');
+    const result = romanDigitValue('M');
     try std.testing.expectEqual(result, 1000);
 }
