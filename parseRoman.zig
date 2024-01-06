c627c30f99748c021bc96327006317920fdd932e
Lasse Becker <lasse@cpsh.de>
2024-01-06 18:33:30 +0100

    typo

--- parseRoman.zig
+++ parseRoman.zig
@@ -10,11 +10,20 @@ pub fn main() !void {
     _ = ArgIterator.skip(argv_p);
     var arg = ArgIterator.next(argv_p) orelse "";
     const value = parseRoman(&arg);
-    try stdout.print("The value in decimal representation is, {d}!\n", .{value});
+    try stdout.print("The value in decimal representation is: {d}\n", .{value});
 }
 
-pub fn parseRoman(input: *[:0]const u8) *[]const u8 {
-    return input;
+pub fn parseRoman(input: *[:0]const u8) i32 {
+    var result: i32 = 0;
+    var prev: i32 = 0;
+    var curr: i32 = 0;
+    for (input.*) |byte| {
+        curr = romanDigit(byte);
+        const negate: i32 = @intFromBool(prev < curr);
+        result += prev - negate * 2 * prev;
+        prev = curr;
+    }
+    return result + curr;
 }
 
 fn romanDigit(char: u8) i32 {
@@ -25,18 +34,6 @@ fn romanDigit(char: u8) i32 {
     const b2 = c & 2;
     const b1 = c & 1;
     return -2129 + @divFloor(c * 743 + mod * 4412 - b4 * 1890 + b3 * 4835 - b2 * 12175 + b1 * 32845, 40);
-}
-
-test "Sandbox" {
-    const i: i32 = 73; // 1001001
-    try std.testing.expectEqual(@mod(i, 10), 3);
-    try std.testing.expectEqual(i & 64, 64);
-    try std.testing.expectEqual(i & 32, 0);
-    try std.testing.expectEqual(i & 16, 0);
-    try std.testing.expectEqual(i & 8, 8);
-    try std.testing.expectEqual(i & 4, 0);
-    try std.testing.expectEqual(i & 2, 0);
-    try std.testing.expectEqual(i & 1, 1);
 }
 
 test "I = 1" {
